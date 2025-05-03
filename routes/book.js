f411b536d765d9ac4f49d62ba0a51aa3c34a67ee
Mathias Wakgari <104831041+mathiaswakgari@users.noreply.github.com>
2025-05-03 13:04:21 +0300

    feat(routes): add RESTful routes for Book API and connect to controller.

---
+++ routes/book.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const {
+  createBook,
+  getAllBooks,
+  getBookById,
+  updateBook,
+  deleteBook,
+} = require("../controllers/book");
+
+router.post("/", createBook);
+
+router.get("/", getAllBooks);
+
+router.get("/:id", getBookById);
+
+router.put("/:id", updateBook);
+
+router.delete("/:id", deleteBook);
+
+module.exports = router;
--- startup/routes.js
+++ startup/routes.js
@@ -1,3 +1,7 @@
+const bookRoutes = require("../routes/book");
+
 module.exports = function (app) {
   app.get("/", (req, res) => res.send("Welcome to CI/CD."));
+
+  app.use("/api/books", bookRoutes);
 };
